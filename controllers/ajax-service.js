c274985a68e45e2effc6be3d7044069dde161193
dibaroy <43357657+dibaroy@users.noreply.github.com>
2019-06-30 00:50:03 -0400

    Added images and updated ajax-service & component JS files

--- controllers/ajax-service.js
+++ controllers/ajax-service.js
@@ -1,2 +1,38 @@
-// Write your generic AjaxService code here.
-// The generic AjaxService should collect data from the JSON file using AJAX.
+/*
+A Service is a controller which could be used to handle any type of
+data processing but is commonly used to fetch or save data.
+*/
+export class AjaxService {
+  /*
+  To collect data from the JSON file we will need to make a generic AJAX call in
+  a static method called ajax().
+
+  This method will have a parameter which will represent the JSON file and
+  a parameter called insertedCode which we will call as a function
+  inside of the AJAX call if it is successful.
+  */
+  static ajax(fileName, insertedCode) {
+    /*
+    To create a new instance of the XMLHttpRequest constructor and set the
+    onstatereadychange event to an anonymous function which will check to
+    ensure that the readyState property has reached stage 4 and that the
+    status property returns a 200 OK response code.
+    */
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        insertedCode(this.responseText);
+      }
+    };
+    /*
+    Call the open() method using the GET request method, the name of the JSON
+    file and a true boolean to indicate that this is being done asyncronously
+    as parameters.
+    */
+    xhttp.open('GET', fileName, true);
+
+    // To complete the AJAX call we need to call the send() method.
+    xhttp.send();
+  }
+}
--- controllers/component.js
+++ controllers/component.js
@@ -1,4 +1,55 @@
-// Write your generic Component code here.
-// The generic Component should define a selector property which selects an
-// HTML element and a service property which is an instance of a service class
-// or a child of a service class.
+/*
+A component is a controller which is used to control a portion of the view.
+
+It holds properties and methods which are intended to be used in the
+HTML or CSS which is associated with this portion of the view.
+*/
+import { AjaxService } from '../controllers/ajax-service.js';
+/*
+This class will have a selector property which will be used to select an HTML
+element to display this component's data in and a service property which will be
+used to store a service which relates to this component.
+*/
+const _SELECTOR = Symbol('selector');
+const _SERVICE = Symbol('service');
+
+export class Component {
+  constructor(selector, service) {
+    this.selector = selector;
+    this.service = service;
+  }
+  /*
+  This class will use getters and setters as well. The setter for the
+  selector property will check to see if the value is a string. If yes, then it
+  will use the value as the parameter in document.getElementById('id') to
+  select an element with a matching ID.
+  */
+  set selector(value) {
+    if (typeof value == 'string') {
+      this[_SELECTOR] = document.getElementById(value);
+    } else {
+      console.error('The selector must be string!');
+    }
+  }
+
+  get selector() {
+    return this[_SELECTOR];
+  }
+
+  /*
+  The setter for the service property will check to ensure that the
+  dunder proto of the value is equal to the AjaxService class to ensure
+  that the supplied service is child of the primary AJAX class.
+  */
+  set service(value) {
+    if (value.__proto__ == AjaxService) {
+      this[_SERVICE] = value;
+    } else {
+      console.error('The service must be a child of the AjaxService class!');
+    }
+  }
+
+  get service() {
+    return this[_SERVICE];
+  }
+}
